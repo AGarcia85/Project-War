f3b834b3be7f1f586a02724412c37e676a945908
AlexGarcia <afgarcia666@gmail.com>
2019-11-22 08:32:41 -0500

    started class Cards

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 console.log("War Baby!");
+
+//1. first create the class
+//2. construct its properties
+
+class Cards {
+    constructor(suit, rank, value) {
+        this.suit = suit,
+        this.rank = rank,
+        this.value = value,
+    }
+}
+

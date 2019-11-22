f8081cada3e4a8a56bab8f3a82025b0dd03141c8
AlexGarcia <afgarcia666@gmail.com>
2019-11-22 08:40:33 -0500

    Added Deck class and props

--- main.js
+++ main.js
@@ -1,13 +1,42 @@
 console.log("War Baby!");
 
-//1. first create the class
-//2. construct its properties
+//1. first create the class*
+//2. construct its properties*
 
 class Cards {
     constructor(suit, rank, value) {
         this.suit = suit,
         this.rank = rank,
-        this.value = value,
+        this.value = value
     }
 }
 
+//3. create a new class called Deck*
+//4. add value to the properties*
+
+class Deck {
+    constructor() {
+        this.cards = []
+            let suits = [
+                "Clubs",
+                "Diamonds",
+                "Hearts",
+                "Spades"
+            ]
+            let rank = [
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9,
+                "Jack",
+                "Queen",
+                "King",
+                "Ace"
+            ]
+    }
+        
+}
